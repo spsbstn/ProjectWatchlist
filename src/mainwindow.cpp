ee836138c09a0a777960610ca4857ec5b3110933
Sebastian <sebastian_spanner@hotmail.com>
2012-09-14 16:24:14 +0200

    minor codeCleanup

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -6,7 +6,6 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
-
     ui->setupUi(this);
     // init MainView
     MainView v;
@@ -18,7 +17,6 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
-
 
 //show window and render data
 void MainWindow::init() {
@@ -43,12 +41,6 @@ void MainWindow::init() {
 void MainWindow::on_actionAdd_triggered()
 {
     qDebug() << "AddSeries clicked";
-
     v.addTile("Test",2,3,"cool",4,2);
 
-    /*
-    *TODO:
-    *remove lines 26 & 27
-    *for cleaner entry
-    */
 }
