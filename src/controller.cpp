28c6cb90f050d7e4c48958bb637ec5bbeb3313c4
Sebastian <sebastian_spanner@hotmail.com>
2012-11-30 21:12:58 +0100

    added Comments to controller.cpp

--- src/controller.cpp
+++ src/controller.cpp
@@ -54,8 +54,10 @@ Controller::Controller(QObject *parent) :
         framelessHelper->setWidgetMovable(false);
         framelessHelper->setWidgetResizable(true);
 
-
+    //  setup quickInfoObject
         qi = new QuickInfo(this);
+
+    // register quickInfo Signals and Slots
         QObject *rootObject = dynamic_cast<QObject*>(qmlView->rootObject());
         QObject::connect(rootObject, SIGNAL(xmlDataRequired(QString)), qi, SLOT(createConnection(QString)));
         QObject::connect(qi->xmlPicture_, SIGNAL(updateFinished()), rootObject, SLOT(updateInfo()));
@@ -63,12 +65,14 @@ Controller::Controller(QObject *parent) :
 
 }
 
+// add new show
 void Controller::add(const QString& name, const QString& genre)
 {
     db->data->addShow(name, genre);
     db->addShow(name,genre);
 }
 
+// remove show
 void Controller::remove(const QString& name){
 
     db->data->removeShow(name);
@@ -76,40 +80,52 @@ void Controller::remove(const QString& name){
 }
 
 
-// delta is either +1 or -1
+// change season, delta is either +1 or -1
 void Controller::setSeason(const QString& name, int delta)
 {
     db->data->setSeason(name, delta);
     db->alterSeason(name,delta);
 }
 
+// change Episode
 void Controller::setEpisode(const QString &name, int delta)
 {
     db->data->setEpisode(name, delta);
     db->alterEpisode(name,delta);
 }
 
+//change colorscheme
 void Controller::changeColorScheme(const QString &color,const QString &schemeName){
 
-
+    // change frame color
      mainWidget->setStyleSheet("background:'"+color+"'");
+
+     // save current scheme name and hexColor to settings
      settings->setValue("colorScheme", schemeName);
      settings->setValue("color",color);
 
 }
 
+//load colorScheme
 QString Controller::loadColorScheme(){
 
+    // load name und hexColor from settingsfile
     QString schemeName = settings->value("colorScheme").toString();
     QString color = settings->value("color").toString();
+
+    // change frame color
     mainWidget->setStyleSheet("background:'"+color+"'");
+
+    // return name of scheme
     return schemeName;
 
 }
 
 
+// check if app starts for the first time --> use default color scheme
 void Controller::checkForFirstInit(){
 
+    // check ich colorScheme information exists
     if(settings->contains("colorScheme")){
 
         return;
@@ -124,51 +140,52 @@ void Controller::checkForFirstInit(){
 
 }
 
+//maximize window
 void Controller::windowMaximize()
 {
     mainWidget->setGeometry(QApplication::desktop()->availableGeometry());
 }
 
 
+// minimize window
 void Controller::windowMinimize()
 {
     mainWidget->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter,
                                                 mainWidget->minimumSize(), QApplication::desktop()->availableGeometry()));
 }
 
+//hide Window
 void Controller::windowHide()
 {
     mainWidget->showMinimized();
 }
 
+// calculate frameColor according to backgroundColor and alpha
 void Controller::alphaBlendFrame(QString color, double alpha)
 {
 
-
+    // sepearte hexString
     QString hexRed=color.mid(1,2);
     QString hexGreen=color.mid(3,2);
     QString hexBlue=color.mid(5,2);
 
-    //qDebug() << hexRed << hexGreen << hexBlue;
+    bool ok; // useless
 
-    bool ok;
-
+    // convert hexStrings to decimalNumbers
     int red=hexRed.toInt(&ok, 16);
     int green=hexGreen.toInt(&ok, 16);
     int blue=hexBlue.toInt(&ok, 16);
 
-    //qDebug() << red << green << blue;
-
-    //blend black (0,0,0) over color
+    //blend black (rgba(0,0,0)) over color (A*alpha + (1-alpha)*B)
 
     red = (1-alpha)*red+1;
     green = (1-alpha)*green+1;
     blue = (1-alpha)*blue+1;
 
+    //create hexString
     QString blendedColor = QColor(red,green,blue,1 ).name();
 
-    //qDebug() << blendedColor;
-
+    //change frame color
     mainWidget->setStyleSheet("background:'"+blendedColor+"'");
 
 }
