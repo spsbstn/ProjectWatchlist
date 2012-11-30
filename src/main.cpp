75a02f777f8839fdcc723f533ff5e1e7db4cd197
Sebastian <sebastian_spanner@hotmail.com>
2012-11-30 21:27:15 +0100

    added Comments to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,28 @@
 
 int main(int argc, char *argv[])
 {
+    // set applicationName
     QApplication::setApplicationName("Watchlist");
+
+    // switch to different graphicSystem for better performance
     QApplication::setGraphicsSystem(QLatin1String("raster"));
+
+    //init app
     QApplication app(argc, argv);
+
+    //init controller
     Controller* ctrl = new Controller;
+
+    // connect quit signal
     QObject::connect((QObject*)ctrl->qmlView->engine(), SIGNAL(quit()), &app, SLOT(quit()));
 
+    //maximize Window
     ctrl->windowMaximize();
+
+    //show Window
     ctrl->mainWidget->show();
+
+    //center Window (obsolete ? )
     ctrl->mainWidget->setGeometry(QStyle::alignedRect(Qt::LeftToRight, Qt::AlignCenter,
                           ctrl->mainWidget->size(), app.desktop()->availableGeometry()) );
 
