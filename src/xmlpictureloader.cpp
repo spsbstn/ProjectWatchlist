58cc455ec161a98b6319f1397b74a54b86a1dd2d
Sebastian <sebastian_spanner@hotmail.com>
2012-11-30 21:24:16 +0100

    added Comments to xmlpictureloader.cpp

--- src/xmlpictureloader.cpp
+++ src/xmlpictureloader.cpp
@@ -6,29 +6,40 @@
 XmlPictureLoader::XmlPictureLoader(QObject *parent) :
     QObject(parent)
 {
+    // create networkAccessManager
     nam = new QNetworkAccessManager(this);
+
+    //connect signal and slot  --> request finished
     QObject::connect(nam, SIGNAL(finished(QNetworkReply*)),this, SLOT(finishedSlot(QNetworkReply*)));
 }
 
+// create connection to server
 void XmlPictureLoader::createConnection(QString showId)
 {
+    //open url (base + showNId)
     QUrl url(URL_BASE+ showId);
+
+    // serverReply
     QNetworkReply* reply = nam->get(QNetworkRequest(url));
 }
 
+// work with serverReply
 void XmlPictureLoader::finishedSlot(QNetworkReply* reply)
 {
 
     if (reply->error() == QNetworkReply::NoError)
     {
 
+         //convert reply to QDomDocument
         QByteArray bytes = reply->readAll();
         QString string = QString::fromUtf8(bytes);;
         QDomDocument doc;
         doc.setContent(string);
 
-        //access elements
+        //access elements by tag
         list=doc.elementsByTagName("image");
+
+        // get imageUrl
         imageUrl=list.at(0).toElement().text();
 
     }
@@ -38,6 +49,9 @@ void XmlPictureLoader::finishedSlot(QNetworkReply* reply)
         qDebug() << "ERROR: Http-Error occurred";
     }
 
+    // emit signal that all data is retrieved
     emit updateFinished();
+
+    // delete reply
     reply->deleteLater();
 }
